1a0778bb72a20898172f743a1b3ae59cd6ce76f2
Idris <icoder112@gmail.com>
2020-02-17 20:38:04 -0500

    updates

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chrono/Desktop/Projects_Root/Web-Dev-Projects/rp-portfolio/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
